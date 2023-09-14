e1f38f420813971341cfbc891c9e128332aad3fa
Chris Pulman <chris.pulman@yahoo.com>
2023-09-15 00:36:33 +0100

    Update Build tools with SDK fix

--- build/Build.cs
+++ build/Build.cs
@@ -49,7 +49,7 @@ partial class Build : NukeBuild
 
     Target Clean => _ => _
         .Before(Restore)
-        .Executes(() =>
+        .Executes(async () =>
         {
             if (!IsLocalBuild)
             {
@@ -57,7 +57,7 @@ partial class Build : NukeBuild
             }
 
             PackagesDirectory.CreateOrCleanDirectory();
-            ////await this.InstallDotNetSdk("3.1.x", "5.x.x", "6.x.x", "7.x.x");
+            await this.InstallDotNetSdk("3.1.x", "5.x.x", "6.x.x", "7.x.x");
         });
 
     Target Restore => _ => _
